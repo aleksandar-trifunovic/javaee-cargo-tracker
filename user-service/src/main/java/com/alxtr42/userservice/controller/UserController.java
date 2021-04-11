f94cb963413483cd5fbec2f9930434498b001fed
aleksandar-trifunovic <aleksandar.trifunovic96@outlook.com>
2021-04-11 21:02:13 +0200

    Implemented rest controller, domain, repository and service files

---
+++ user-service/src/main/java/com/alxtr42/userservice/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.alxtr42.userservice.controller;
+
+import com.alxtr42.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+}
---
+++ user-service/src/main/java/com/alxtr42/userservice/service/UserService.java
@@ -0,0 +1,12 @@
+package com.alxtr42.userservice.service;
+
+import com.alxtr42.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+}

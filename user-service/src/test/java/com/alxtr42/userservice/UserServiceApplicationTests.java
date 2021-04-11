5717f97daf0fea11fe998ef8832e6163661afccd
aleksandar-trifunovic <aleksandar.trifunovic96@outlook.com>
2021-04-11 20:42:58 +0200

    Spring Init User Service

---
+++ user-service/src/test/java/com/alxtr42/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alxtr42.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7af133fb681593126a618b3037351f1d98b327d8
aleksandar-trifunovic <aleksandar.trifunovic96@outlook.com>
2021-04-11 19:33:56 +0200

    Added loggers and changed port number

--- department-service/src/main/java/com/alxtr42/department/controller/DepartmentController.java
+++ department-service/src/main/java/com/alxtr42/department/controller/DepartmentController.java
@@ -2,11 +2,13 @@ package com.alxtr42.department.controller;
 
 import com.alxtr42.department.domain.Department;
 import com.alxtr42.department.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/departments")
+@Slf4j
 public class DepartmentController {
 
     @Autowired
@@ -14,11 +16,13 @@ public class DepartmentController {
 
     @PostMapping("/")
     public Department saveDepartment(@RequestBody Department department) {
+        log.info("Inside saveDepartment method of Department Controller");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/{id}")
     public Department findDepartmentById(@PathVariable("id") Long departmentId) {
+        log.info("Inside findDepartmentById method of Department Controller");
         return departmentService.findDepartmentById(departmentId);
     }
 }
--- department-service/src/main/java/com/alxtr42/department/service/DepartmentService.java
+++ department-service/src/main/java/com/alxtr42/department/service/DepartmentService.java
@@ -2,20 +2,24 @@ package com.alxtr42.department.service;
 
 import com.alxtr42.department.domain.Department;
 import com.alxtr42.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class DepartmentService {
 
     @Autowired
     private DepartmentRepository departmentRepository;
 
     public Department saveDepartment(Department department) {
+        log.info("Inside saveDepartment method of Department Service");
         return departmentRepository.save(department);
     }
 
     public Department findDepartmentById(Long departmentId) {
+        log.info("Inside findDepartmentById method of Department Service");
         return departmentRepository.findByDepartmentId(departmentId);
     }
 }

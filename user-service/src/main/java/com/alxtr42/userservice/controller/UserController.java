27ebfc170d52101f91a4bd9258c9aefc293d4ea9
aleksandar-trifunovic <aleksandar.trifunovic96@outlook.com>
2021-04-11 21:05:13 +0200

    Implemented endpoints for User controller and a method to save user

--- user-service/src/main/java/com/alxtr42/userservice/controller/UserController.java
+++ user-service/src/main/java/com/alxtr42/userservice/controller/UserController.java
@@ -1,12 +1,22 @@
 package com.alxtr42.userservice.controller;
 
+import com.alxtr42.userservice.domain.User;
 import com.alxtr42.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @PostMapping("/")
+    public User saveUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
 }
--- user-service/src/main/java/com/alxtr42/userservice/service/UserService.java
+++ user-service/src/main/java/com/alxtr42/userservice/service/UserService.java
@@ -1,5 +1,6 @@
 package com.alxtr42.userservice.service;
 
+import com.alxtr42.userservice.domain.User;
 import com.alxtr42.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 }

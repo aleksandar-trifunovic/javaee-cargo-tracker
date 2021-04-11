f59cd2502eca8903aefaee6077cd3abd1a631b1c
aleksandar-trifunovic <aleksandar.trifunovic96@outlook.com>
2021-04-11 21:21:58 +0200

    Added logger for service and controller, implemented value-object to connect User and Department

--- user-service/src/main/java/com/alxtr42/userservice/controller/UserController.java
+++ user-service/src/main/java/com/alxtr42/userservice/controller/UserController.java
@@ -2,6 +2,7 @@ package com.alxtr42.userservice.controller;
 
 import com.alxtr42.userservice.domain.User;
 import com.alxtr42.userservice.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -17,6 +19,9 @@ public class UserController {
 
     @PostMapping("/")
     public User saveUser(@RequestBody User user) {
+        log.info("Inside saveUser method of UserController");
         return userService.saveUser(user);
     }
+
+
 }
--- user-service/src/main/java/com/alxtr42/userservice/service/UserService.java
+++ user-service/src/main/java/com/alxtr42/userservice/service/UserService.java
@@ -2,16 +2,19 @@ package com.alxtr42.userservice.service;
 
 import com.alxtr42.userservice.domain.User;
 import com.alxtr42.userservice.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
     public User saveUser(User user) {
+        log.info("Inside saveUser method of UserRepository");
         return userRepository.save(user);
     }
 }
